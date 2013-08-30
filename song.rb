3ae1c192636a6579c376074dd59f3ad7739b9e2b
silvia <via_s@hotmail.com>
2013-08-29 21:18:18 -0700

    First bits!

---
+++ song.rb
@@ -0,0 +1,32 @@
+require 'yaml'
+
+class Song
+  attr_accessor :title, :artist, :album, :track, :notes
+  def to_s
+    output = "Title: #{@title}\n"
+    output << "Artist: #{@artist}\n"
+    output << "Album: #{album}\n"
+    output << "Track: # {@track}\n"
+    output
+   end
+
+  def to_yaml
+    the_song ={
+      title: @title,
+      artist: @artist,
+      album: @album,
+      track: @tract
+   }
+
+   YAML.dump(the_song)
+
+  end
+
+  def dump (filename=nil)
+   filename ||= @title
+   filename ||= 'unknown'
+
+   File.write("#{filename}.yml", to_yaml)
+  end
+
+end
